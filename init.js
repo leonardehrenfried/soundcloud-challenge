55bd36f52edce4da094b49feddd6d4150eabd7e9
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2012-01-28 15:28:30 +0100

    Refactor login code

--- init.js
+++ init.js
@@ -1,21 +1,29 @@
-//initialise Soundcloud client library
-SC.initialize({
-  client_id    : "fb4ca313af415c9ce6a0e18478d16224",
-  redirect_uri : "http://lenni.info/soundcloud-challenge/callback.html"
-});
-
 $(document).ready(function(){
-  
-  $("#login").click(function(){
-    SC.connect(function(){
-      $("#login-box").remove();
-      $("#playlists-box").removeClass("hidden");
-      UI.refreshPlaylistList();
-    });
+    
+  var startApp = function(){
+    $("#login-box").remove();
+    $("#playlists-box").removeClass("hidden");
+    UI.refreshPlaylistList();
+  };
+
+  //initialise Soundcloud client library
+  SC.initialize({
+    client_id    : "fb4ca313af415c9ce6a0e18478d16224",
+    redirect_uri : "http://lenni.info/soundcloud-challenge/callback.html"
   });
+
+  if(SC.accessToken()){
+    startApp();
+  }
+  else{
+    $("#login").click(function(){
+      SC.connect(function(){
+        startApp();
+      });
+    });
+  }
 
   $("#add-playlist").click(function(){
     UI.createPlaylist();  
   });
-
 });
