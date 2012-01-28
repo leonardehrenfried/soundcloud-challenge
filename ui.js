e5bc8a3267c424de2bceceb93f8cb5dad821d219
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2012-01-28 14:12:13 +0100

    Persist title changes and refactor code

--- ui.js
+++ ui.js
@@ -1,5 +1,10 @@
 UI={
-  
+  //list of playlists, I would have preferred not to store them locally 
+  //and keep fetching them from the server on every update but it seems
+  //that the data changes need a few minutes to propagate, hence the local
+  //cache
+  playlists : [],
+
   /**
    * Fetches playlists from the API and displays them on the left hand side
    * menu.
@@ -7,18 +12,32 @@ UI={
   refreshPlaylistList : function(){
     var that = this;
     SC.get("/me/playlists",{}, function(resp, err){
-      var playlists = $("#playlists");
-      playlists.empty();
-      $.each(resp, function(i, playlist){
-        var li = $("<li>").text(playlist.title)
-                  .click(function(){
-                    that.showPlaylist(playlist); //I ♥ closures  
-                  });
-        playlists.append(li);
-      });
+      if(err){
+        that.showNotification("Could not fetch playlists: " + err.message);
+      }
+      else{
+        that.playlists = resp;
+        that.renderPlaylists(that.playlists);
+      }
     }) 
   },
   
+  /**
+   * Renders the list of playlists on the left hand side.
+   */
+  renderPlaylists : function(playlists){
+    var that = this,
+        playlistContainer = $("#playlists").empty();
+
+    $.each(playlists, function(i, playlist){
+      var li = $("<li>").text(playlist.title)
+      .click(function(){
+        that.showPlaylist(playlist); //I ♥ closures  
+      });
+      playlistContainer.append(li);
+    }); 
+  },
+
   /**
    * Displays a playlist in the right hand panel.
    */
@@ -26,28 +45,51 @@ UI={
     var that = this;
     var playlistContainer = $("#playlist-detail").empty();
     var editButton = $("<button>").addClass("btn").text("Edit title")
-                      .click(function(){
-                        var newTitle = prompt("Enter new title:");
-                        playlist.title = newTitle;
-                        that.showPlaylist(playlist);
-                      });
+    .click(function(){
+      var newTitle = prompt("Enter new title:");
+      if(newTitle){
+        playlist.title = newTitle;
+        that.savePlaylistTitle(playlist); 
+        that.showPlaylist(playlist);
+        that.renderPlaylists(that.playlists);
+      }
+    });
     var h3 = $("<h3>").text(playlist.title)
-              .append(editButton);
+    .append(editButton);
     playlistContainer.append(h3);
 
     if(playlist.tracks.length < 0){
-    
+
     }
     else{
       playlistContainer.append($("<div>")
-                       .text("This playlist doesn't contain any tracks yet."))
+                               .text("This playlist doesn't contain any tracks yet."))
     }
   },
-  
+
+  /**
+   * Persist the name change of a playlist
+*/
+  savePlaylistTitle : function(playlist){
+    SC.post("/me/playlists", 
+            {
+              playlist:{
+                id:    playlist.id,
+                title: playlist.title
+              }
+            }, 
+            function(response, error){
+              if(error){
+                that.showNotification("Saving your playlist failed:"+error.message);
+                console.log(error);
+              }
+            });
+  },
+
   /**
    * Creates a new, empty playlist. The user needs change the name and 
    * fill it with tracks afterwards.
-   */
+*/
   createPlaylist : function(){
     var that = this;
     SC.post("/playlists.json", {
@@ -68,9 +110,9 @@ UI={
     });
   },
 
-  /*
+/*
    * Displays a notfication to the user.
-   */
+*/
   showNotification : function(text){
     var notifications = $("#notifications").removeClass("hidden");
     notifications.find("p").text(text);
